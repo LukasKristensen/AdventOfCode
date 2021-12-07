f2adb2a26efb2b5c882155ecc316cbb7c7845c11
DKtruster <lukashtxb16@gmail.com>
2021-12-07 17:56:29 +0100

    dec7

---
+++ 7.py
@@ -0,0 +1,10 @@
+with open('7.txt') as d:
+    data = (d.read().splitlines())[0].split(",")
+    data = [int(toString) for toString in data]
+
+    smallestSum = 0
+    for x in range (max(data)):
+        sumX = [abs(int(i)-int(x)) for i in data]
+        if sum(sumX)<smallestSum or smallestSum == 0: smallestSum=sum(sumX)
+    print(smallestSum)
+
