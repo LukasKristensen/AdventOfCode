b3eeb1371f08e973617980e2588182c9d225c9f2
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-01 10:00:11 +0100

    1b

--- 2022/1dec/day1.py
+++ 2022/1dec/day1.py
@@ -1,11 +1,22 @@
-max_count = 0
 current_count = 0
+elves = []
 
 for i in open('day1_data.txt').read().splitlines():
     if i != '':
         current_count+=int(i)
     else:
-        if current_count > max_count:
-            max_count = current_count
+        elves.append(current_count)
         current_count = 0
-print("Max count:",max_count)
+
+# 1a
+print("1a)",max(elves))
+
+# 1b
+summed_three_most = 0
+for i in range(3):
+    biggest_val = max(elves)
+    elves.remove(biggest_val)
+    summed_three_most+=biggest_val
+print("1b)",summed_three_most)
+
+
