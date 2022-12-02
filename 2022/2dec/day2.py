cff80eb6a7abdf0fbdabbda678b548ca38b1b93e
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-02 10:04:04 +0100

    Update day2.py

--- 2022/2dec/day2.py
+++ 2022/2dec/day2.py
@@ -1,16 +1,24 @@
-shape_to_score = {'A': 1, 'B': 2, 'C':3, 'X': 1, 'Y': 2, 'Z':3}
+shape_to_score = {'X': 1, 'Y': 2, 'Z':3, 'rock': 1, 'paper': 2, 'scissors':3}
 outcome_decider = {'AX': 'draw', 'AY': 'won', 'AZ':'lose','BX':'lose'
     ,'BY':'draw','BZ':'won','CX':'won','CY':'lose','CZ':'draw'}
-outcome_to_score = {'lose': 0, 'draw': 3, 'won':6}
+outcome_to_score = {'lose': 0, 'draw': 3, 'won':6, 'X': 0, 'Y': 3, 'Z':6}
+shape_decider = {'AX': 'scissors', 'AY': 'rock', 'AZ':'paper','BX':'rock'
+    ,'BY':'paper','BZ':'scissors','CX':'paper','CY':'scissors','CZ':'rock'}
 
 score = 0
 score_second = 0
 for i in open('day2_data.txt').read().splitlines():
     opponent_play, your_play = i.split(' ')
+
+    # 2A
     score+=shape_to_score[your_play]
     score+=outcome_to_score[outcome_decider[opponent_play+your_play]]
 
+    # 2B
+    score_second+=shape_to_score[shape_decider[opponent_play+your_play]]
+    score_second+=outcome_to_score[your_play]
 
+print("2a):",score)
+print("2b):",score_second)
 
-print("Your score:",score)
 
