14ec8cddc84bfca5dd0a7a9f27a8e6673f2ee2e4
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-03 10:56:52 +0100

    3b

--- 2022/3dec/day3.py
+++ 2022/3dec/day3.py
@@ -1,18 +1,20 @@
-print(ord("a"))
-print(ord("b"))
-print(ord("A"))
-
-print("A".islower())
-
 score = 0
 for i in open('day3_data.txt').read().splitlines():
-    first_bag, second_bag = i[:round(len(i)/2)], i[round(len(i)/2):]
+    first_bag, second_bag = i[round(len(i)/2):], i[:round(len(i)/2)]
     for x in first_bag:
         if x in second_bag:
-            if x.islower():
-                score+=ord(x)-96
-            else:
-                score+=ord(x)-38
+            score += ord(x)-96 if x.islower() else ord(x)-38
             break
 print("3a)",score)
+
+
+score = 0
+data = open('day3_data.txt').read().splitlines()
+for i in range(round(len(data)/3)):
+    for x in data[i*3]:
+        if x in data[i*3+1] and x in data[i*3+2]:
+            score += ord(x)-96 if x.islower() else ord(x)-38
+            break
+print("3b)",score)
+
 
