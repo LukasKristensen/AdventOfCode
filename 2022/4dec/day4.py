fbd4f6037f60edb8ff2df428c0793d4bad00e986
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-04 07:11:30 +0100

    4b

--- 2022/4dec/day4.py
+++ 2022/4dec/day4.py
@@ -1,4 +1,5 @@
 score = 0
+second_score = 0
 
 for i in open('day4_data.txt').read().splitlines():
     first_range, second_range = i.split(',')
@@ -8,6 +9,9 @@ for i in open('day4_data.txt').read().splitlines():
     if (a <= c <= d <= b) or (c <= a <= b <= d):
         score += 1
 
+    if a <= d and c <= b:
+        second_score += 1
 
 print("4a)", score)
+print("4b)", second_score)
 
