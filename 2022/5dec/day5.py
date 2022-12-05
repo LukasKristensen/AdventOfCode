71ed571ecac96c220ee6c82a71aaa45693d0926b
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-05 16:12:08 +0100

    Day 5b

--- 2022/5dec/day5.py
+++ 2022/5dec/day5.py
@@ -1,13 +1,14 @@
+import copy
 data = open('day5_data.txt').read().splitlines()
 crates = {}
+crates_second = {}
 
 for i in range(round(len(data[0])/4)):
     crates[i] = []
 
-data_sorted = False
 for i in range(len(data)):
     if data[i] != "":
-        if not data_sorted:
+        if not crates_second:
             for x in range(len(crates)):
                 if '[' in data[i][x*4:(x+1)*4]:
                     crates[x].append(data[i][x*4:(x+1)*4].replace(' ',''))
@@ -15,15 +16,18 @@ for i in range(len(data)):
             _, amount, _, before, _, after = data[i].split(" ")
             for y in range(int(amount)):
                 crates[int(after)-1].append(crates[int(before)-1].pop(-1))
-            crates = crates[::-1]
-            data_sorted = True
+            crates_second[int(after)-1]+=crates_second[int(before)-1][-int(amount):]
+            crates_second[int(before)-1] = crates_second[int(before)-1][:-int(amount)]
     else:
         for crate in range(len(crates)): crates[crate] = crates[crate][::-1]
+        crates_second = copy.deepcopy(crates)
 
-result_a = ""
+result_a, result_b = "", ""
 for x in crates:
     result_a += crates[x][-1].replace('[', '').replace(']', '')
+    result_b += crates_second[x][-1].replace('[', '').replace(']', '')
 print("5a)",result_a)
+print("5b)",result_b)
 
 
 
