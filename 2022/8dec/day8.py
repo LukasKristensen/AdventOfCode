76f9a6130a3c27c38cbc6be47138ba74a3ed4b2e
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-08 17:52:08 +0100

    Update day8.py

--- 2022/8dec/day8.py
+++ 2022/8dec/day8.py
@@ -1,54 +1,52 @@
-data = open('day8_validation.txt').read().splitlines()
+data = open('day8_data.txt').read().splitlines()
 formatted = []
+trees_visible = 0
+max_scenic_score = 0
 
 for i in range(len(data)):
     formatted.append([])
     for x in range(len(data[0])):
         formatted[i].append(int(data[i][x]))
 
-data = formatted
-print("Formatted:",formatted)
-
-print("Showed")
-for z in formatted:
-    print(z)
-
-trees_visible = 0
-
-for y in range(1, len(data) - 1):
-    for x in range(1, len(data[0])-1):
-        up_visible = True
-        down_visible = True
-        left_visible = True
-        right_visible = True
-
-        print("Data:",data)
-
-        current_value = data[y][x]
-        left_side = data[y][:x]
-        right_side = data[y][x+1:]
+for y in range(1, len(formatted) - 1):
+    for x in range(1, len(formatted[0]) - 1):
+        current_value = formatted[y][x]
+        left_side = formatted[y][:x]
+        right_side = formatted[y][x + 1:]
         up_side = []
         down_side = []
 
-        print("Coordinate:","X:",x,"Y:",y)
-        print("Left side:",left_side)
-        print("Right side:",right_side)
-
         for up in range(y):
-            up_side.append(data[y-up-1][x])
-        for down in range(y, len(data[0])-y):
-            down_side.append(data[y+down][x])
-
-        print("Up side:",up_side)
-        print("down side:",down_side)
-
-        visible = False
+            up_side.append(formatted[y - up - 1][x])
+        for down in range(y, len(formatted[0]) - 1):
+            down_side.append(formatted[down + 1][x])
         for i in [left_side, right_side, down_side, up_side]:
-            if current_value < max(i):
+            if current_value > max(i):
                 trees_visible += 1
                 break
 
+        s_right, s_left, s_up, s_down = 0,0,0,0
+        for i in right_side:
+            s_right += 1
+            if current_value <= i:
+                break
+        for i in down_side:
+            s_down += 1
+            if current_value <= i:
+                break
+        for i in left_side[::-1]:
+            s_left += 1
+            if current_value <= i:
+                break
+        for i in up_side:
+            s_up += 1
+            if current_value <= i:
+                break
+        scenic_score = s_up*s_down*s_left*s_right
+        if scenic_score > max_scenic_score:
+            max_scenic_score = scenic_score
 
-print("Summed visible:",trees_visible)
+print("8a)", trees_visible + len(formatted) * 2 + len(formatted[0]) * 2 - 4)
+print("8b)", max_scenic_score)
 
-# 7855 too high
+# 235200
