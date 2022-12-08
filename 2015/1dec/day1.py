5500c96df175c14e8e0134fa1f19fc187e23c04c
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-08 20:42:03 +0100

    1b

--- 2015/1dec/day1.py
+++ 2015/1dec/day1.py
@@ -1,2 +1,12 @@
 print("1a)",sum([1 if i == "(" else -1 for i in open('day1_data.txt').read().splitlines()[0]]))
 
+floor, i = 0, 0
+data = open('day1_data.txt').read().splitlines()[0]
+while floor > -1:
+    if data[i] == "(":
+        floor += 1
+    else:
+        floor -= 1
+    i += 1
+print("1b)",i)
+
