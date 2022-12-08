dd5b9c89e57b31e9162640dbbada566f9db5d54f
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-08 21:15:26 +0100

    2b

--- 2015/2dec/day2.py
+++ 2015/2dec/day2.py
@@ -1,8 +1,8 @@
-data = open('day2_data.txt').read().splitlines()
-paper_wrap = 0
-for i in data:
+paper_wrap, ribbon = 0, 0
+for i in open('day2_data.txt').read().splitlines():
     l, w, h = map(int, i.split("x"))
     paper_wrap += 2*l*w+2*w*h+2*h*l+min([l*w,l*h,w*h])
-print("1a)",paper_wrap)
+    ribbon += 2*sorted([l, w, h])[1]+2*sorted([l, w, h])[0]+w*l*h
+print("2a)",paper_wrap)
+print("2b)",ribbon)
 
-# 1731664 too high
