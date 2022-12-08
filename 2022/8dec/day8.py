2b22f1b629052e058284e334b7fbea1d71f281ae
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-08 11:19:24 +0100

    Update day8.py

--- 2022/8dec/day8.py
+++ 2022/8dec/day8.py
@@ -1,34 +1,53 @@
 data = open('day8_validation.txt').read().splitlines()
+formatted = []
+
+for i in range(len(data)):
+    formatted.append([])
+    for x in range(len(data[0])):
+        formatted[i].append(int(data[i][x]))
+
+data = formatted
+print("Formatted:",formatted)
+
+print("Showed")
+for z in formatted:
+    print(z)
+
 trees_visible = 0
 
 for y in range(1, len(data) - 1):
-
     for x in range(1, len(data[0])-1):
         up_visible = True
         down_visible = True
         left_visible = True
         right_visible = True
 
-        for up in range(1, y+1):
-            if data[x][y-up-1] >= data[x][y]:
-                up_visible = False
+        print("Data:",data)
+
+        current_value = data[y][x]
+        left_side = data[y][:x]
+        right_side = data[y][x+1:]
+        up_side = []
+        down_side = []
+
+        print("Coordinate:","X:",x,"Y:",y)
+        print("Left side:",left_side)
+        print("Right side:",right_side)
+
+        for up in range(y):
+            up_side.append(data[y-up-1][x])
+        for down in range(y, len(data[0])-y):
+            down_side.append(data[y+down][x])
+
+        print("Up side:",up_side)
+        print("down side:",down_side)
+
+        visible = False
+        for i in [left_side, right_side, down_side, up_side]:
+            if current_value < max(i):
+                trees_visible += 1
                 break
-        for down in range(y, len(data)-y-1):
-            if data[x][y+down] >= data[x][y]:
-                down_visible = False
-                break
-        for left in range(1, x+1):
-            print("[LEFT]","X:", x, "Y:", y, "Val:", data[x][y],"Checking left:",data[x-left][y],"Left:",left)
-            if data[x-left][y] >= data[x][y]:
-                left_visible = False
-                break
-        for right in range(x, len(data[0])-x-1):
-            if data[x+right][y] >= data[x][y]:
-                right_visible = False
-                break
-        print("X:",x,"Y:",y,"Val:",data[x][y],up_visible,down_visible,left_visible,right_visible)
-        if up_visible or down_visible or left_visible or right_visible:
-            trees_visible+=1
+
 
 print("Summed visible:",trees_visible)
 
