1699fe13b6b5577e863ee3df8fb69ec0dec3a515
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-10 15:09:09 +0100

    Better color coded output readability

--- 2022/10dec/day10.py
+++ 2022/10dec/day10.py
@@ -1,4 +1,3 @@
-data = open('day10_data.txt').read().splitlines()
 cpu_cycles = [-20+x*40 for x in range(1, 6)]
 screen = ["" for x in range(6)]
 summed_signal_strength, value, cycle, row = 0, 1, 0, -1
@@ -9,20 +8,19 @@ def draw():
     if (cycle / 40).is_integer():
         row += 1
     if value - 1 <= cycle % 40 <= value + 1:
-        screen[row] += "\033[32m#"
+        screen[row] += "\033[32m▓▓"
     else:
-        screen[row] += "\033[31m."
+        screen[row] += "\033[31m░░"
     cycle += 1
     if cycle in cpu_cycles:
         summed_signal_strength += value * cycle
 
 
-for i in data:
+for i in open('day10_data.txt').read().splitlines():
     draw()
     if i[:4] == "addx":
         draw()
         value += int(i.split(" ")[1])
 
-print("10a)", summed_signal_strength)
-print("10b)",*screen,sep="\n")
+print(f'10a) {summed_signal_strength}', "10b)",*screen,sep="\n")
 
