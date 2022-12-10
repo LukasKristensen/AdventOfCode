e0f858e81f141de55e4ecc0861a8f65bbe69730d
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-12-10 14:13:31 +0100

    10b

--- 2022/10dec/day10.py
+++ 2022/10dec/day10.py
@@ -1,26 +1,56 @@
 data = open('day10_data.txt').read().splitlines()
-cycles_check = [20, 60, 100, 140, 180, 220]
-
+cpu_cycles = [20, 60, 100, 140, 180, 220]
 summed_signal_strength = 0
 value = 1
 cycle = 1
+
 for i in data:
     command = i[:4]
     if command == "addx":
         cycle += 1
-        if cycle in cycles_check:
+        if cycle in cpu_cycles:
             summed_signal_strength += value*cycle
         cycle += 1
         value += int(i.split(" ")[1])
-        if cycle in cycles_check:
+        if cycle in cpu_cycles:
             summed_signal_strength += value*cycle
     else:
         cycle += 1
-        if cycle in cycles_check:
+        if cycle in cpu_cycles:
             summed_signal_strength += value*cycle
-print(summed_signal_strength)
+print("10a)", summed_signal_strength)
+
+screen = ["#", "", "", "", "", "", "","",""]
+
+sprite = 1
+cycle = 1
+row = 0
 
 
-# 14040 too low
-# 14760 too high
+def draw():
+    global row
+    if (cycle/40).is_integer():
+        row += 1
+    if sprite-1 <= cycle%40 <= sprite+1:
+        screen[row] += "#"
+    else:
+        screen[row] += "."
+
+
+for i in data:
+    command = i[:4]
+    if command == "addx":
+        draw()
+        cycle += 1
+        sprite += int(i.split(" ")[1])
+        draw()
+        cycle += 1
+    else:
+        draw()
+        cycle += 1
+
+print("10b)")
+for row in screen:
+    print(row)
+
 
