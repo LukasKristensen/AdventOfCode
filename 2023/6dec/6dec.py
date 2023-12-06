8ee08992dd76dfdc0a83aa1aa549516d2a5f1f37
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-12-06 06:55:25 +0100

    Update 6dec.py

--- 2023/6dec/6dec.py
+++ 2023/6dec/6dec.py
@@ -7,14 +7,14 @@ def race_simulate(time, distance):
     result = 1
     for race in range(len(time)):
         options = 0
-        for i in range(int(time[race])):
-            record = i*(int(time[race])-i)
-            if int(distance[race]) < record:
+        for i in range(time[race]):
+            record = i*(time[race]-i)
+            if distance[race] < record:
                 options += 1
         result *= options if options > 0 else 1
     return result
 
 
 print("Part one:", race_simulate(time, distance))
-print("Part two:", race_simulate(["".join(str(x) for x in time)], ["".join(str(x) for x in distance)]))
+print("Part two:", race_simulate([int("".join(str(x) for x in time))], [int("".join(str(x) for x in distance))]))
 
