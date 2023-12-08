24565c85d63d4f8789f587939ca3d65ab063bb22
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-12-08 07:53:22 +0100

    Update 8dec.py

--- 2023/8dec/8dec.py
+++ 2023/8dec/8dec.py
@@ -6,49 +6,33 @@ mapped_instructions = {}
 
 for i in instructions:
     key, val = i.split(' = ')
-    val = val[1:-1]
-    mapped_instructions[key] = val.split(", ")
+    mapped_instructions[key] = val[1:-1].split(", ")
 
 current_location = 'AAA'
-step_count_p1, step_count_p2 = 0, 0
-
+step_count1, step_count2 = 0, 0
 part_one = None
-search = True
 
-while search:
-    if current_location == 'ZZZ' and not part_one:
-        part_one = step_count_p1
-        print("Part one:", part_one)
-        search = False
+while not part_one:
+    if current_location == 'ZZZ':
+        part_one = step_count1
     for i in navigation:
-        step_count_p1 += 1
+        step_count1 += 1
         if i == 'L':
             current_location = mapped_instructions[current_location][0]
         elif i == 'R':
             current_location = mapped_instructions[current_location][1]
+print("Part one:", part_one)
 
-
-step_count_2 = 0
-testing = True
 index_true = {}
-
-locations_with_a = []
-for i in mapped_instructions:
-    if i[-1] == 'A':
-        locations_with_a.append(i)
+locations_with_a = [i for i in mapped_instructions if i[-1] == 'A']
 locations_from_a = locations_with_a.copy()
 
-while testing:
+while len(index_true) != len(locations_with_a):
     for i in navigation:
         for g in range(len(locations_from_a)):
             if locations_from_a[g][-1] == 'Z' and locations_with_a[g] not in index_true:
-                index_true[locations_with_a[g]] = step_count_2
-
-        print("Checking if:", len(index_true), len(locations_with_a))
-        if len(index_true) == len(locations_with_a):
-            testing = False
-            break
-        step_count_2 += 1
+                index_true[locations_with_a[g]] = step_count2
+        step_count2 += 1
 
         for x in range(len(locations_from_a)):
             if i == 'L':
@@ -56,13 +40,11 @@ while testing:
             elif i == 'R':
                 locations_from_a[x] = mapped_instructions[locations_from_a[x]][1]
 
-print("indexed:", index_true)
 
-print("locations:", locations_with_a)
 running_multi = [index_true[x] for x in index_true]
-print("running multi:", running_multi)
-
 lcm = running_multi[0]
+
 for num in running_multi[1:]:
     lcm = lcm * num // math.gcd(lcm, num)
-print("part 2:", lcm)
+
+print("Part two:", lcm)
