74f4a5268fc2607adf1c675161e5e1d439aaff95
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-12-08 08:36:47 +0100

    Update 8dec.py

--- 2023/8dec/8dec.py
+++ 2023/8dec/8dec.py
@@ -1,50 +1,38 @@
 import math
 data_load = open("input.txt", "r").read().splitlines()
-navigation = data_load[0]
-instructions = data_load[2:]
+navigation, instructions = data_load[0], data_load[2:]
 mapped_instructions = {}
 
 for i in instructions:
     key, val = i.split(' = ')
-    mapped_instructions[key] = val[1:-1].split(", ")
+    mapped_instructions[key] = {'L': val[1:-1].split(", ")[0], 'R': val[1:-1].split(", ")[1]}
 
 current_location = 'AAA'
 step_count1, step_count2 = 0, 0
-part_one = None
 
-while not part_one:
-    if current_location == 'ZZZ':
-        part_one = step_count1
+while current_location != 'ZZZ':
     for i in navigation:
         step_count1 += 1
-        if i == 'L':
-            current_location = mapped_instructions[current_location][0]
-        elif i == 'R':
-            current_location = mapped_instructions[current_location][1]
-print("Part one:", part_one)
+        current_location = mapped_instructions[current_location][i]
 
-index_true = {}
-locations_with_a = [i for i in mapped_instructions if i[-1] == 'A']
-locations_from_a = locations_with_a.copy()
+print("Part one:", step_count1)
 
-while len(index_true) != len(locations_with_a):
+locations = [{'origin': i, 'new': i, 'seed': 0} for i in mapped_instructions if i[-1] == 'A']
+
+while 0 in [loc['seed'] for loc in locations]:
     for i in navigation:
-        for g in range(len(locations_from_a)):
-            if locations_from_a[g][-1] == 'Z' and locations_with_a[g] not in index_true:
-                index_true[locations_with_a[g]] = step_count2
+        for g in range(len(locations)):
+            if locations[g]['new'][-1] == 'Z' and locations[g]['seed'] == 0:
+                locations[g]['seed'] = step_count2
         step_count2 += 1
 
-        for x in range(len(locations_from_a)):
-            if i == 'L':
-                locations_from_a[x] = mapped_instructions[locations_from_a[x]][0]
-            elif i == 'R':
-                locations_from_a[x] = mapped_instructions[locations_from_a[x]][1]
+        for x in range(len(locations)):
+            locations[x]['new'] = mapped_instructions[locations[x]['new']][i]
 
+collected_seeds = [x['seed'] for x in locations]
+lcm = collected_seeds[0]
 
-running_multi = [index_true[x] for x in index_true]
-lcm = running_multi[0]
-
-for num in running_multi[1:]:
+for num in collected_seeds[1:]:
     lcm = lcm * num // math.gcd(lcm, num)
 
 print("Part two:", lcm)
