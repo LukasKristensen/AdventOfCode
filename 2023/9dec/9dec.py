d76b47bf1845ef418cf2a5497295969199beafed
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-12-09 06:50:57 +0100

    Day 9 - Python

---
+++ 2023/9dec/9dec.py
@@ -0,0 +1,20 @@
+data_load = open("input.txt", "r").read().splitlines()
+data_formatted = [list(map(int, i.split(" "))) for i in data_load]
+
+final_differences = []
+for i in range(len(data_formatted)):
+    differences = [data_formatted[i]]
+    while differences[-1] != [0]*len(differences[-1]):
+        difference_iteration = []
+        for x in range(1, len(differences[-1])):
+            difference_iteration.append(int(differences[-1][x])-int(differences[-1][x-1]))
+        differences.append(difference_iteration)
+    final_differences.append(differences)
+
+for i in range(len(final_differences)):
+    for x in range(1, len(final_differences[i])):
+        final_differences[i][-1-x].append(final_differences[i][-1-x][-1]+final_differences[i][-x][-1])
+        final_differences[i][-1-x] = [final_differences[i][-1-x][0]-final_differences[i][-x][0]] + final_differences[i][-1-x]
+
+print("Part one:", sum([i[0][-1] for i in final_differences]))
+print("Part two:", sum([i[0][0] for i in final_differences]))
