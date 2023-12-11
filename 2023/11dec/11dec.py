a84e538098d359ca2b9badae2ef137f82ee2c373
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-12-11 08:49:12 +0100

    Update 11dec.py

--- 2023/11dec/11dec.py
+++ 2023/11dec/11dec.py
@@ -16,41 +16,22 @@ for x in range(len(data[0])):
         if y == len(data)-1:
             indexes_empty_x.append(x)
 
-
-locations = []
-for y in range(len(data)):
-    for x in range(len(data[y])):
-        if data[y][x] == "#":
-            locations.append((x, y))
-
-
-pairs = []
+locations = [(x, y) for x in range(len(data[0])) for y in range(len(data)) if data[y][x] == "#"]
 distance = 0
 additional_multiply = 0
+checked_locations = []
+
 for i in locations:
     for j in locations:
-        if i != j and (i, j) not in pairs and (j, i) not in pairs:
-            print(i, j)
-            if i[0] <= j[0]:
-                for x in range(i[0], j[0]+1):
-                    if x in indexes_empty_x:
-                        additional_multiply += 1
-            else:
-                for x in range(j[0], i[0]+1):
-                    if x in indexes_empty_x:
-                        additional_multiply += 1
-            if i[1] <= j[1]:
-                for y in range(i[1], j[1]+1):
-                    if y in indexes_empty_y:
-                        additional_multiply += 1
-            else:
-                for y in range(j[1], i[1]+1):
-                    if y in indexes_empty_y:
-                        additional_multiply += 1
-
+        if i != j and (i, j) not in checked_locations and (j, i) not in checked_locations:
+            additional_multiply += sum([1 for x in range(i[0], j[0]) if x in indexes_empty_x and i[0] <= j[0]])
+            additional_multiply += sum([1 for x in range(j[0], i[0]) if x in indexes_empty_x and i[0] > j[0]])
+            additional_multiply += sum([1 for y in range(i[1], j[1]) if y in indexes_empty_y and i[1] <= j[1]])
+            additional_multiply += sum([1 for y in range(j[1], i[1]) if y in indexes_empty_y and i[1] > j[1]])
+            checked_locations.append((i, j))
             distance += abs(i[0] - j[0]) + abs(i[1] - j[1])
-            pairs.append((i, j))
 
+print("additional_multiply: ", additional_multiply)
 print("part_one: ", distance+additional_multiply)
 print("part_two: ", distance+(additional_multiply*1000000)-additional_multiply)
 
